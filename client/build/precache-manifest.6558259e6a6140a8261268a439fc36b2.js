7bfe14890426dfc8c7821abdcd84a308c74ebb8a
Yirbett Joseph <yirbett@gmail.com>
2020-10-25 11:45:56 -0400

    deploy to heroku

---
+++ client/build/precache-manifest.6558259e6a6140a8261268a439fc36b2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11d491b316c4d499b657f369209aa95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1df4a782c61caa7022e2",
+    "url": "/static/css/main.5c33d493.chunk.css"
+  },
+  {
+    "revision": "80ce3c1e815992581a82",
+    "url": "/static/js/2.32f6e7ee.chunk.js"
+  },
+  {
+    "revision": "b144d5e96c59f8711b6f903b54c52dfc",
+    "url": "/static/js/2.32f6e7ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df4a782c61caa7022e2",
+    "url": "/static/js/main.1faa9d5b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b409cc011e013de2fcd303aebb946015",
+    "url": "/static/media/Budget.b409cc01.PNG"
+  },
+  {
+    "revision": "0c02d0aff649cb2b2d8485372b973941",
+    "url": "/static/media/Dashboard.0c02d0af.png"
+  },
+  {
+    "revision": "ca5ecf9737b379efc7c2165ef752b2ab",
+    "url": "/static/media/PlanerPage.ca5ecf97.png"
+  },
+  {
+    "revision": "332f54300ca29bafc3e6787b9b469980",
+    "url": "/static/media/Preview.332f5430.PNG"
+  },
+  {
+    "revision": "04fe5279ea240374a965c40096268e6d",
+    "url": "/static/media/SelectProjectName.04fe5279.png"
+  },
+  {
+    "revision": "e4c0df4900b73bf08f0186a44c504a78",
+    "url": "/static/media/banner2.e4c0df49.jpg"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const helmet = require("helmet");
-const morgan = require("morgan");
+const favicon = require("express-favicon");
+const path = require("path");
+const port = process.env.PORT || 8080;
 const app = express();
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server Listening on port ${port}`));
+app.use(favicon(__dirname + "/build/favicon.ico"));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/ping", function (req, res) {
+  return res.send("pong");
+});
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
