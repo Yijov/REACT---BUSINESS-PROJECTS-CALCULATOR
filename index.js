d31995855f2a8569d45d0ed99de080d378c747a0
Yirbett Joseph <yirbett@gmail.com>
2020-10-26 06:01:47 -0400

    deploy to heroku

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const favicon = require("express-favicon");
+
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-app.listen(port);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+app.listen(port, () => console.log(`Now runing on port ${port}`));
